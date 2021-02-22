2489b4b92729b33d1fb6a0b0bf962828110bb854
ohhyunmin <skemfdl1010@naver.com>
2021-02-22 12:02:55 +0900

    Section modify

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -15,7 +15,7 @@ class Main extends Component {
             도전하는 개발자
           </h1><br/>
           <section className="App-main-About-Section">
-            현재 저는 .Net S/W 개발 2년차입니다.<br/>
+            현재 저는 .Net S/W 개발 3년차입니다.<br/>
             다양한 웹 사이트 ( ESM2.0, 티몬, 롯데ON 등 ) 의
             API 와 Script 를 분석하며, Web에 대한 관심이 높아졌고
             관심이 높아진 만큼 도전해보고 싶다는 마음이 들었습니다.<br/><br/>
@@ -24,7 +24,7 @@ class Main extends Component {
             간단한 게시판을 구현해보았으며, C# 외의 언어를 통해
             구현해보고 싶은 마음에 React, NodeJS 등 을 
             이용해 간단한 Mini Project 를 진행해보았습니다.<br/><br/>
-            더 나아가 특정 언어에 제약 받지 않는 개발자가
+            더 나아가 새로운 기술을 습득하며, 특정 언어에 제약 받지 않는 개발자가
             되어가는 것이 저의 목표입니다.
           </section>
         </div>
