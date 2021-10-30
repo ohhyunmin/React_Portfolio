8b1d0540d278fbe9f038f09843f207b301e5b084
NoPln <66984443+ohhyunmin@users.noreply.github.com>
2021-10-30 21:41:30 +0900

    Update ReadPortfolioList.js

--- src/Components/ReadPortfolioList.js
+++ src/Components/ReadPortfolioList.js
@@ -9,9 +9,9 @@ class ReadPortfolioList extends Component {
     super(props);
     this.state = {
       portfolioList : [{id:1, title:'고객 관리 시스템', image:crm,
-      desc:'고객 관리 시스템 Clone 코딩을 통해 웹사이트를 구현해보았습니다.', skill:"React, NodeJS, AWS-RDS (My-SQL), Material-UI", link:'https://github.com/ohhyunmin/React_Portfolio'},
+      desc:'고객 관리 시스템 Clone 코딩을 통해 웹사이트를 구현해보았습니다.', skill:"React, NodeJS, AWS-RDS (My-SQL), Material-UI", link:'https://github.com/ohhyunmin/React_Crm'},
       {id:2, title:'ReactPortfolio', image:reactPortfolio,
-      desc:'React를 이용해 간단한 포트폴리오 페이지를 만들어보았습니다.', skill:"React", link:'https://github.com/ohhyunmin/ReactPortfolio'},
+      desc:'React를 이용해 간단한 포트폴리오 페이지를 만들어보았습니다.', skill:"React", link:'https://github.com/ohhyunmin/React_Portfolio'},
       {id:3, title:'YoutubeClone', image:youtubeClone, skill:"HTML, CSS, Javescript",
       desc:'디자인에 대한 지식이 부족해 어떤 디자인으로 Web Page 를 만들어 볼까 고민을 하는 중 YoutubeClone 코딩이라는 컨텐츠를 보게 되었습니다. 이 컨텐츠로 처음  동적인 Web Page 를 만들어 보았고, 해당 컨텐츠를 만드신 분의 다양한 컨텐츠를 통해 HTML, CSS, JS 에 대한 지식이 굉장히 많이 향상되었습니다.', link:'https://github.com/ohhyunmin/YoutubeClone'}
       ]
